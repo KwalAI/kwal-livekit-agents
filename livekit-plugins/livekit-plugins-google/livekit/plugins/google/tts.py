38b73c554560b0a52fd1c3117123b42d4f71676f
fjprobos <fjproboste@gmail.com>
2024-05-21 12:00:17 -0700

    Refactoring google tts to implement SynthetizeStream - WIP

--- livekit-plugins/livekit-plugins-google/livekit/plugins/google/tts.py
+++ livekit-plugins/livekit-plugins-google/livekit/plugins/google/tts.py
@@ -12,17 +12,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 from typing import AsyncIterable, Optional, Union
+import asyncio
+import contextlib
 
 from livekit import rtc
-from livekit.agents import codecs, tts
+from livekit.agents import codecs, aio, tts
 
 from google.cloud import texttospeech
 from google.cloud.texttospeech_v1.types import (
     SsmlVoiceGender,
     SynthesizeSpeechResponse,
 )
+
+from gtts import gTTS
 
 from .log import logger
 from .models import AudioEncoding, Gender, SpeechLanguages
@@ -141,3 +145,246 @@ class TTS(tts.TTS):
 
         except Exception as e:
             logger.error(f"failed to synthesize: {e}")
+
+    def stream(
+        self,
+    ) -> "SynthesizeStream":
+        return SynthesizeStream(self._session, self._opts)
+
+
+class SynthesizeStream(tts.SynthesizeStream):
+    _STREAM_EOS = ""
+
+    def __init__(
+        self,
+        opts: TTSOptions,
+        max_retry: int = 32,
+    ):
+        self._opts = opts
+
+        self._queue = asyncio.Queue[str | None]()
+        self._event_queue = asyncio.Queue[tts.SynthesisEvent | None]()
+        self._closed = False
+        self._text = ""
+
+        self._main_task = asyncio.create_task(self._run(max_retry))
+
+    def _stream_url(self) -> str:
+        base_url = self._opts.base_url
+        voice_id = self._opts.voice.id
+        model_id = self._opts.model_id
+        sample_rate = self._opts.sample_rate
+        latency = self._opts.latency
+        return f"{base_url}/text-to-speech/{voice_id}/stream-input?model_id={model_id}&output_format=pcm_{sample_rate}&optimize_streaming_latency={latency}"
+
+    def push_text(self, token: str | None) -> None:
+        """
+        Push some text to internal queue to be consumed by _run method.
+        """
+        if self._closed:
+            raise ValueError("cannot push to a closed stream")
+
+        if token is None:
+            self._flush_if_needed()
+            return
+
+        if len(token) == 0:
+            # 11labs marks the EOS with an empty string, avoid users from pushing empty strings
+            return
+
+        # TODO: Naive word boundary detection may not be good enough for all languages
+        # fmt: off
+        splitters = (".", ",", "?", "!", ";", ":", "—", "-", "(", ")", "[", "]", "}", " ")
+        # fmt: on
+
+        self._text += token
+
+        while True:
+            last_split = -1
+            for i, c in enumerate(self._text):
+                if c in splitters:
+                    last_split = i
+                    break
+
+            if last_split == -1:
+                break
+
+            seg = self._text[: last_split + 1]
+            seg = seg.strip() + " "  # 11labs expects a space at the end
+            self._queue.put_nowait(seg)
+            self._text = self._text[last_split + 1 :]
+
+    async def aclose(self, *, wait: bool = True) -> None:
+        self._flush_if_needed()
+        self._queue.put_nowait(None)
+        self._closed = True
+
+        if not wait:
+            self._main_task.cancel()
+
+        with contextlib.suppress(asyncio.CancelledError):
+            await self._main_task
+
+    def _flush_if_needed(self) -> None:
+        seg = self._text.strip()
+        if len(seg) > 0:
+            self._queue.put_nowait(seg + " ")
+
+        self._text = ""
+        self._queue.put_nowait(SynthesizeStream._STREAM_EOS)
+
+    async def _run(self, max_retry: int) -> None:
+        retry_count = 0
+        tts_task: asyncio.Task | None = None
+        data_tx: aio.ChanSender[str] | None = None
+
+        try:
+            while True:
+                try:
+                    data = await self._queue.get()
+
+                    if data is None:
+                        break
+
+                    if data == SynthesizeStream._STREAM_EOS:
+                        continue
+
+                    data_tx, data_rx = aio.channel()
+                    #for idx, decoded in enumerate(gtts.stream()):
+                        #fp.write(decoded)
+                        #log.debug("part-%i written to %s", idx, fp)
+                    tts_task = asyncio.create_task(self._run_tts(data_rx))
+
+                    assert data_tx is not None
+                    assert tts_task is not None
+
+                    data_tx.send_nowait(data) # Correctly sendin this data
+
+                except Exception:
+                    if retry_count >= max_retry:
+                        logger.exception(
+                            f"failed to connect to 11labs after {max_retry} retries"
+                        )
+                        break
+
+                    retry_delay = min(retry_count * 5, 5)  # max 5s
+                    retry_count += 1
+
+                    logger.warning(
+                        f"failed to connect to 11labs, retrying in {retry_delay}s"
+                    )
+                    await asyncio.sleep(retry_delay)
+
+        except Exception:
+            logger.exception("11labs task failed")
+        finally:
+            with contextlib.suppress(asyncio.CancelledError):
+                if tts_task is not None:
+                    tts_task.cancel()
+                    await tts_task
+
+            self._event_queue.put_nowait(None)
+
+    async def _run_tts(
+        self, data_rx: aio.ChanReceiver[str]
+    ) -> None:
+        closing_ws = False
+
+        self._event_queue.put_nowait(
+            tts.SynthesisEvent(type=tts.SynthesisEventType.STARTED)
+        )
+
+        async def send_task():
+            nonlocal closing_ws
+
+            # 11labs stream must be initialized with a space
+            voice = self._opts.voice
+            voice_settings = (
+                asdict(voice.settings) if voice.settings else None
+            )
+            #init_pkt = dict(
+            #    text=" ",
+            #    voice_settings=voice_settings,
+            #)
+            #await ws.send_str(json.dumps(init_pkt))
+
+            while True:
+
+                data = await data_rx.recv()
+                data_pkt = dict(
+                    text=data,
+                    try_trigger_generation=True,
+                )
+                if data == SynthesizeStream._STREAM_EOS:
+                    closing_ws = True
+
+                await ws.send_str(json.dumps(data_pkt))
+
+                if closing_ws:
+                    return
+
+        async def recv_task():
+            nonlocal closing_ws
+            while True:
+                msg = await ws.receive()
+                if msg.type in (
+                    aiohttp.WSMsgType.CLOSED,
+                    aiohttp.WSMsgType.CLOSE,
+                    aiohttp.WSMsgType.CLOSING,
+                ):
+                    if closing_ws:  # close is expected
+                        return
+
+                    raise Exception("11labs connection closed unexpectedly")
+
+                if msg.type != aiohttp.WSMsgType.TEXT:
+                    logger.warning("unexpected 11labs message type %s", msg.type)
+                    continue
+
+                data: dict = json.loads(msg.data)
+                if data.get("audio"):
+                    b64data = base64.b64decode(data["audio"])
+                    frame = rtc.AudioFrame(
+                        data=b64data,
+                        sample_rate=self._opts.sample_rate,
+                        num_channels=1,
+                        samples_per_channel=len(b64data) // 2,
+                    )
+                    self._event_queue.put_nowait(
+                        tts.SynthesisEvent(
+                            type=tts.SynthesisEventType.AUDIO,
+                            audio=tts.SynthesizedAudio(text="", data=frame),
+                        )
+                    )
+                elif data.get("isFinal"):
+                    return
+
+        try:
+            await asyncio.gather(send_task(), recv_task())
+        except Exception:
+            logger.exception("11labs connection failed")
+        finally:
+            self._event_queue.put_nowait(
+                tts.SynthesisEvent(type=tts.SynthesisEventType.FINISHED)
+            )
+
+    async def __anext__(self) -> tts.SynthesisEvent:
+        evt = await self._event_queue.get()
+        if evt is None:
+            raise StopAsyncIteration
+
+        return evt
+
+
+def dict_to_voices_list(data: dict) -> List[Voice]:
+    voices = []
+    for voice in data["voices"]:
+        voices.append(
+            Voice(
+                id=voice["voice_id"],
+                name=voice["name"],
+                category=voice["category"],
+                settings=None,
+            )
+        )
+    return voices
